72b814c627c4b70536142623eb3bcea511bcb882
Hakki Kirmizi <hakki@yahoo-inc.com>
2015-08-06 15:37:10 +0800

    share image via mms feature

--- app/src/main/java/com/hakkicaner/imgsearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/hakkicaner/imgsearch/activities/ImageDisplayActivity.java
@@ -1,19 +1,31 @@
 package com.hakkicaner.imgsearch.activities;
 
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
 
 import com.hakkicaner.imgsearch.R;
 import com.hakkicaner.imgsearch.models.ImageResult;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageDisplayActivity extends ActionBarActivity {
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +43,51 @@ public class ImageDisplayActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_image_display, menu);
         return true;
     }
+
+
+    public void onShareItem(MenuItem m) {
+        // Get access to bitmap image from view
+        ImageView ivImageResult = (ImageView) findViewById(R.id.ivFullDisplay);
+        // Get access to the URI for the bitmap
+        Uri bmpUri = getLocalBitmapUri(ivImageResult);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            Toast.makeText(getBaseContext(), R.string.share_fail, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
